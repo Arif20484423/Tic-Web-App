c39c3da948c36c7e5d4726298480fa2ffc9fdfb5
Md Arif <arif.20527@sscbs.du.ac.in>
2025-04-02 13:03:18 +0530

    merge check

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>main branch push checks 2</div>;
+  return <div>main branch push checks 2 an update to check merge</div>;
 }
