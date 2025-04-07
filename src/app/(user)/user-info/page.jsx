f8c7a5ee8e2682623dd61d672985a889c44df003
the-sudip <mailofsudipdas@gmail.com>
2025-04-08 00:44:46 +0530

    Input component for user info added

---
+++ src/app/(user)/user-info/page.jsx
@@ -0,0 +1,13 @@
+"use client"
+import React, {useState} from 'react'
+import Input from '@/components/user-info/Input'
+const page = () => {
+    const [email, setEmail] = useState("");
+  return (
+    <>
+      <Input size="large" label="Enter Email" priority="required" type="email" placeholder="Enter email here" value={email} setValue={setEmail}></Input>
+    </>
+  )
+}
+
+export default page
