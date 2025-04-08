d13e71ab4004720c02968b3f3866b66de08d9865
the-sudip <mailofsudipdas@gmail.com>
2025-04-08 23:49:12 +0530

    User details structure created

--- src/app/(user)/user-info/page.jsx
+++ src/app/(user)/user-info/page.jsx
@@ -2,13 +2,15 @@
 import React, {useState} from 'react'
 import Input from '@/components/user-info/Input'
 import Button from '@/components/user-info/Button'
+import UserDetails from '@/components/user-info/UserDetails'
 const page = () => {
     const clickhandler = () => {
         console.log("Button clicked")
     }
   return (
     <>
-      <Button size="large" variant="filled" onClick={clickhandler}>Click here!</Button>
+      {/* <Button size="large" variant="filled" onClick={clickhandler}>Click here!</Button> */}
+      <UserDetails></UserDetails>
     </>
   )
 }
---
+++ src/components/user-info/NavbarOptions.jsx
@@ -0,0 +1,9 @@
+'use client'
+import React from "react"
+const NavbarOptions = () =>{
+    return(
+        <>
+        </>
+    )
+}
+export default NavbarOptions;
