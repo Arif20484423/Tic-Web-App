324ee2a16cf79c8af62d5d86a5dd0829c896c1a1
P00P.head <tullu20top@gmail.com>
2025-04-10 03:15:41 +0530

    added sidebar, header and corrected user form

--- src/components/user-info/SideNavbar.jsx
+++ src/components/user-info/SideNavbar.jsx
@@ -4,7 +4,11 @@ import NavbarOptions from "./NavbarOptions";
 const SideNavbar = () => {
     return(
         <>
-        <div className="h-screen w-[220px] bg-amber-800">Side navbar</div>
+        
+        <div className="h-screen w-[220px] bg-[var(--primaryColor-500)] px-[20px] py-[50px] flex flex-col gap-y-[100px]">
+            <div className="text-[24px] text-[var(--textColor-white)] font-bold ">TIC</div>
+            <NavbarOptions />
+        </div>
         </>
     )
 }
--- src/components/user-info/UserDetails.jsx
+++ src/components/user-info/UserDetails.jsx
@@ -5,8 +5,7 @@ import Input from "./Input";
 import Button from "./Button";
 import RadioInput from "./RadioInput";
 const UserDetails = () => {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [altEmail, setAltEmail] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
@@ -26,13 +25,38 @@ const UserDetails = () => {
 
     return (
         <>
-        <div className=" flex flex-row h-full w-full">
+        <div className=" flex flex-row h-full w-full bg-[var(--blackColor-050)]">
+            {/* sidebar            */}
             <SideNavbar></SideNavbar>
-            <div className=" flex-1 w-min-[400px] ">
-                <div className="flex h-[50px] w-full bg-amber-300">Header</div>
-                     <div className="grid grid-cols-2 gap-4 p-4 w-[80%] m-auto bg-[#4A4A4A]">
-                        <Input size="large" label="First Name" priority="required" type="text" placeholder="Enter Details" value={firstName} setValue={setFirstName}/>
-                        <Input size="large" label="Last Name" priority="required" type="text" placeholder="Enter Details" value={lastName} setValue={setLastName}/>
+            {/* main content */}
+            <div className="flex-1 w-min-[400px] ">
+                {/* header */}
+                <div className="flex justify-between item-center h-[70px] w-full bg-[var(--blackColor-050)] px-[30px] py-[10px]">
+                        <div id="title">
+                            <div className="text-[20px] font-bold text-[var(--primaryColor-500)] ">Account Information</div>
+                            <div className="text-[10px] text-[var(--textColor-tertiary)]">&larr; Back</div>
+                        </div>
+                        <div className="flex flex-row items-center gap-x-4" id="hello-user">  
+                                <div className="text-right">
+                                    <div className="text-[17px] font-extrabold text-[var(--primaryColor-500)]">
+                                        Hello, {name}
+                                    </div>
+                                    <div className="text-[10px] text-[var(--textColor-tertiary)]">{rollNumber}</div>
+                                </div>
+                                <div className="w-[50px] h-[50px] flex justify-center items-center rounded-full">
+                                    <img 
+                                    src="https://cdn-icons-png.flaticon.com/512/1144/1144760.png"
+                                    alt="user-icon" 
+                                    className="w-full h-full object-cover" 
+                                    />
+                                </div> 
+                        </div>
+                </div>
+
+                {/* user form */}
+                <div className="flex justify-center h-[calc(100vh-70px)">
+                    <div className="grid grid-cols-2 gap-4 p-4 w-[80%] m-auto bg-[var(--blackColor-050)] ">
+                        <Input size="large" label="Name" priority="required" type="text" placeholder="Enter Details" value={name} setValue={setName}/>
                         <Input size="large" label="Email" priority="optional" type="email" placeholder="Enter Details" value={email} setValue={setEmail}/>
                         <Input size="large" label="Alternative Email" priority="optional" type="email" placeholder="Enter Details" value={altEmail} setValue={setAltEmail}/>
                         <Input size="large" label="Phone Number" priority="required" type="number" placeholder="Enter Details" value={phoneNumber} setValue={setPhoneNumber}/>
@@ -45,6 +69,8 @@ const UserDetails = () => {
                         <Input size="large" label="Languages" priority="optional" type="text" placeholder="Enter Details" value={languages} setValue={setLanguages}/>
                         <Input size="large" label="Domain" priority="optional" type="text" placeholder="Enter Details" value={domain} setValue={setDomain}/>
                         <Input size="large" label="Gap Year" priority="required" type="number" placeholder="Enter Details" value={gapYear} setValue={setGapYear}/>
+                        
+                        <Input size="large" label="Resume" priority="required" type="text" placeholder="Enter Drive Link" value={resume} setValue={setResume}/>
                         <div className="grid grid-cols-2 w-[350px] gap-13">
                         <Input size="small" label="10th" priority="required" type="text" placeholder="Enter Details" value={tenth} setValue={setTenth}/>
                         <Input size="small" label="12th" priority="required" type="text" placeholder="Enter Details" value={twelth} setValue={setTwelth}/>
@@ -53,13 +79,14 @@ const UserDetails = () => {
                         <Input size="small" label="UG" priority="required" type="text" placeholder="Enter Details" value={ug} setValue={setUg}/>
                         <Input size="small" label="PG" priority="required" type="text" placeholder="Enter Details" value={pg} setValue={setPg}/>
                         </div>
-                        <Input size="large" label="Resume" priority="required" type="text" placeholder="Enter Drive Links" value={resume} setValue={setResume}/>
-                        <br/>
-                        <div className="grid grid-cols-2 w-[350px] gap-10">
+                        
+                       
+                        <div className="grid grid-cols-2 w-[350px] gap-[50px]">
                             <Button size = "medium" variant = "filled" children type = "button">Save Changes</Button>
                             <Button size = "medium" variant = "outline" children type = "button">Discard Changes</Button>
                         </div>
-                    </div>                 
+                    </div> 
+                </div>                
             </div>
         </div>
         </>
