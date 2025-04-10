aae719859ea85ae2d70d3f81780e42f22bca2f8f
Md Arif <arif.20527@sscbs.du.ac.in>
2025-04-10 15:00:00 +0530

    layout fixed for secure nested dashboard user-details change-password

---
+++ src/components/user-details/ChangePassword.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ChangePassword from "@/components/user-details/ChangePassword"
+const ChangePassword = () => {
+  return (
+    <ChangePassword/>
+  )
+}
+
+export default ChangePassword
