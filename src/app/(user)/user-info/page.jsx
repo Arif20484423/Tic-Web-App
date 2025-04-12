abb9f5c571a8d7d0f2fe883e39f789e9afa9707d
Md Arif <76733190+Arif20484423@users.noreply.github.com>
2025-04-12 23:16:18 +0530

    Update page.jsx

--- src/app/(user)/user-info/page.jsx
+++ src/app/(user)/user-info/page.jsx
@@ -1,11 +1,11 @@
 "use client"
 import React, {useState} from 'react'
-import Input from '@/components/user-info/Input'
-import Button from '@/components/user-info/Button'
-import UserDetails from '@/components/user-info/UserDetails'
-import Data from '@/components/user-info/Data'
-import DataTable from '@/components/user-info/DataTable'
-import students from '@/components/user-info/student'
+import Input from '@/components/user-details/Input'
+import Button from '@/components/user-details/Button'
+import UserDetails from '@/components/user-details/UserDetails'
+import Data from '@/components/user-details/Data'
+import DataTable from '@/components/user-details/DataTable'
+import students from '@/components/user-details/student'
 const page = () => {
     const clickhandler = () => {
         console.log("Button clicked")
