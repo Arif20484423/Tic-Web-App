70facbf837dff12fbaad6a00de1ee9576e2ddfb9
Md Arif <arif.20527@sscbs.du.ac.in>
2025-04-23 02:49:51 +0530

    backend url fixed

--- src/components/secure/students/Students.jsx
+++ src/components/secure/students/Students.jsx
@@ -77,7 +77,7 @@ const Students = () => {
     async function fetchStudents() {
       try {
         const res = await axios.get(
-          "http://localhost:4000/api/v1/user/students",
+          `${process.env.NEXT_PUBLIC_API_URL}/api/v1/user/students`,
           {
             withCredentials: true,
           }
